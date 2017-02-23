c0bc7f4f19c99ec7376c240ac82c9bda44ae55d5
Jurre <99034766@mydavinci.nl>
2017-02-23 13:31:41 +0100

    changed style of lingo

--- index.php
+++ index.php
@@ -8,21 +8,27 @@
 	<script type="text/javascript" src="index.js"></script>
 </head>
 <body>
-	<p>Kies hier het aantal letters!</p>
-	<input type="radio" name="letterAmount" value="5">5
-	<br/>
-	<input type="radio" name="letterAmount" value="6">6
-	<br/>
-	<input type="radio" name="letterAmount" value="7">7
-	<br/>
-	<input type="radio" name="letterAmount" value="8">8
-	<br/>
-	<br/>
-	<button onclick="letterAmount();createTable();" id="letter">letters</button>
-	<br/>
+	<h1>Lingo</h1>
+	<p>Hallo en welkom bij lingo. Om te beginnen kies u eerst het aantal letters. Vervolgens klikt u op start om te beginnen.</p>
+	<div class="letter_block">
+		<span>Kies hier het aantal letters!</span>
+		<br/>
+		<input type="radio" name="letterAmount" value="5">5
+		<br/>
+		<input type="radio" name="letterAmount" value="6">6
+		<br/>
+		<input type="radio" name="letterAmount" value="7">7
+		<br/>
+		<input type="radio" name="letterAmount" value="8">8
+		<br/>
+		<br/>
+		<button onclick="letterAmount();createTable();" id="letter">start</button>
+		<br/>
+		<br/>
+	</div>
 
 	<table id="lingo_table"></table>
 
-	<input id="antwoord" type="text"/><button onclick="checkLetters()">antwoord</button>
+	<input id="antwoord" type="text" placeholder="Vul hier uw antwoord in..." /><button onclick="checkLetters()">antwoord</button>
 </body>
 </html>
